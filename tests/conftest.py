b27b576795723fa0f2ba9a72480eef53601984b1
Shashanka Prajapati <shashankap95@gmail.com>
2022-11-14 11:25:54 -0500

    Initial commit

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+"""
+    Dummy conftest.py for let_it_go_backend.
+
+    If you don't know what this is for, just leave it empty.
+    Read more about conftest.py under:
+    - https://docs.pytest.org/en/stable/fixture.html
+    - https://docs.pytest.org/en/stable/writing_plugins.html
+"""
+
+# import pytest
