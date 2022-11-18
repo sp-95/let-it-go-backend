11b3064a0bf0692cfd18a2eab3e711e07ae638ce
Shashanka Prajapati <shashankap95@gmail.com>
2022-11-17 23:57:27 -0500

    [fix] Change the signin error message response

--- let_it_go_backend/apps/auth/views.py
+++ let_it_go_backend/apps/auth/views.py
@@ -1,12 +1,21 @@
 from rest_framework import generics, renderers
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from let_it_go_backend.apps.auth.serializers import PasswordChangeSerializer
 
 
 class SignInView(ObtainAuthToken):
     renderer_classes = (renderers.JSONRenderer, renderers.BrowsableAPIRenderer)
+
+    def post(self, request, *args, **kwargs):
+        try:
+            return super().post(request=request, *args, **kwargs)
+        except ValidationError as e:
+            error_messages = e.detail.get("non_field_errors", [])
+            return Response({"detail": " ".join(error_messages)})
 
 
 class PasswordChangeView(generics.UpdateAPIView):
