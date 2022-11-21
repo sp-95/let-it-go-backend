9f0df9ffc112f3cf2ddc07c480df71eab60a1532
Shashanka Prajapati <shashankap95@gmail.com>
2022-11-20 22:29:57 -0500

    [LIG-7] [feat] Add search and filter abilities in the product API

--- let_it_go_backend/apps/product/views.py
+++ let_it_go_backend/apps/product/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, viewsets
 
 from let_it_go_backend.apps.product.models import Product
 from let_it_go_backend.apps.product.permissions import ProductPermission
@@ -9,6 +10,9 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [ProductPermission]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ["category", "condition"]
+    search_fields = ["title", "description", "category"]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
