a8e04c70d9f5cac7da3a8166959bafe1932c4d2a
Shashanka Prajapati <shashankap95@gmail.com>
2022-11-24 12:05:38 -0500

    [feat] Send user details on login

--- let_it_go_backend/apps/auth/views.py
+++ let_it_go_backend/apps/auth/views.py
@@ -1,7 +1,9 @@
 from rest_framework import generics, renderers
+from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from let_it_go_backend.apps.auth.serializers import PasswordChangeSerializer
 
@@ -11,7 +13,19 @@ class SignInView(ObtainAuthToken):
 
     def post(self, request, *args, **kwargs):
         try:
-            return super().post(request=request, *args, **kwargs)
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            user = serializer.validated_data["user"]
+            token, created = Token.objects.get_or_create(user=user)
+            return Response(
+                {
+                    "token": token.key,
+                    "id": user.id,
+                    "first_name": user.first_name,
+                    "last_name": user.last_name,
+                    "email": user.email,
+                }
+            )
         except ValidationError as e:
             e.detail = " ".join(e.detail.get("non_field_errors", []))
             raise e
