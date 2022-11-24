ac7e5ec68db5935c7c295c72963d315cc533848a
Shashanka Prajapati <shashankap95@gmail.com>
2022-11-24 11:27:09 -0500

    [fix] Send owner id instead of owner

--- let_it_go_backend/apps/product/serializers.py
+++ let_it_go_backend/apps/product/serializers.py
@@ -4,11 +4,11 @@ from let_it_go_backend.apps.product.models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    owner_id = serializers.IntegerField(source="owner.id", read_only=True)
     owner_first_name = serializers.CharField(source="owner.first_name", read_only=True)
     owner_last_name = serializers.CharField(source="owner.last_name", read_only=True)
     owner_email = serializers.CharField(source="owner.email", read_only=True)
 
     class Meta:
         model = Product
-        fields = "__all__"
-        read_only_fields = ("owner",)
+        exclude = ["owner"]
