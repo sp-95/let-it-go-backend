0cf91dda80d2a22dbbc77f6f027e770528ff32b1
Shashanka Prajapati <shashankap95@gmail.com>
2022-11-30 01:08:20 -0500

    [fix] Return category name instead of id

--- let_it_go_backend/apps/product/serializers.py
+++ let_it_go_backend/apps/product/serializers.py
@@ -4,6 +4,7 @@ from let_it_go_backend.apps.product.models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    category = serializers.CharField(source="category.name", read_only=True)
     owner_id = serializers.IntegerField(source="owner.id", read_only=True)
     owner_first_name = serializers.CharField(source="owner.first_name", read_only=True)
     owner_last_name = serializers.CharField(source="owner.last_name", read_only=True)
