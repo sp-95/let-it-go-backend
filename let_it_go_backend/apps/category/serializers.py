e3bcf0b2baea33f980fc2da9063ceb126c6926db
Shashanka Prajapati <shashankap95@gmail.com>
2022-11-30 01:54:32 -0500

    [chore] Send id with the category

--- let_it_go_backend/apps/category/serializers.py
+++ let_it_go_backend/apps/category/serializers.py
@@ -6,4 +6,4 @@ from let_it_go_backend.apps.category.models import Category
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ("name",)
+        fields = ("id", "name")
