714ce92b1ecdaee114b6417fef2f6a87d6300a76
Shashanka Prajapati <shashankap95@gmail.com>
2022-11-30 01:53:07 -0500

    [fix] Save category to database on product creation

--- let_it_go_backend/apps/product/views.py
+++ let_it_go_backend/apps/product/views.py
@@ -1,6 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, viewsets
 
+from let_it_go_backend.apps.category.models import Category
 from let_it_go_backend.apps.product.models import Product
 from let_it_go_backend.apps.product.permissions import ProductPermission
 from let_it_go_backend.apps.product.serializers import ProductSerializer
@@ -15,4 +16,5 @@ class ProductViewSet(viewsets.ModelViewSet):
     search_fields = ["title", "description", "category"]
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+        category = Category.objects.get(pk=self.request.data["category"])
+        serializer.save(category=category, owner=self.request.user)
